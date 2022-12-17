764bd90e42e6213ffa06236235ebbe9dc4e5c7cb
LomakinaAD <lomakinand@mail.ru>
2022-12-17 19:29:08 +0400

    Revert "second commit"
    This reverts commit 9aa0107d028b41ad0d5f5efd09c43d42612bc2a4.

--- src/core.spec.js
+++ src/core.spec.js
@@ -106,8 +106,6 @@ describe('Задания core js', () => {
     });
 
     describe('#deepEqual', () => {
-        const dummyFunction = () => {};
-
         it('Возвращает true если объекты равны', () => {
             assert.equal(
                 core.deepEqual(
@@ -130,15 +128,6 @@ describe('Задания core js', () => {
                 ),
                 true
             );
-            assert.equal(
-                core.deepEqual({func: dummyFunction}, {func: dummyFunction}),
-                true
-            );
-            assert.equal(
-                core.deepEqual({a: 'a', b: 'b'}, {b: 'b', a: 'a'}),
-                true
-            );
-            assert.equal(core.deepEqual(NaN, NaN), true);
         });
 
         it('Возвращает false если объекты не равны', () => {
@@ -177,10 +166,6 @@ describe('Задания core js', () => {
             assert.strictEqual(core.deepEqual(0, 1), false);
             assert.strictEqual(core.deepEqual(null, 0), false);
             assert.strictEqual(core.deepEqual(null, undefined), false);
-            assert.equal(
-                core.deepEqual({func: dummyFunction}, {func: () => {}}),
-                false
-            );
         });
     });
 });
